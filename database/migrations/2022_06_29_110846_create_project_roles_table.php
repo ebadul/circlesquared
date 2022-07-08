1e8fe9f9e457fe8e4c696e10d0270bbf5f03df9c
Shahin Islam Fakhrul <shahin2k5@gmail.com>
2022-07-08 16:14:57 +0600

    migrate

--- database/migrations/2022_06_29_110846_create_project_roles_table.php
+++ database/migrations/2022_06_29_110846_create_project_roles_table.php
@@ -15,12 +15,12 @@ class CreateProjectRolesTable extends Migration
     {
         Schema::create('project_roles', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id')->unsigned();
             $table->string('project_id');
             $table->string('role_id');
-            $table->string('user_id');
             $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            //$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
             $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
         });
