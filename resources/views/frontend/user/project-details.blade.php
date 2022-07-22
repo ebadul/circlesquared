1f57c7291a6e0b2c4851bd8ab837e4abedb6b071
Shahin Islam Fakhrul <shahin2k5@gmail.com>
2022-07-22 20:39:37 +0600

    update project details

--- resources/views/frontend/user/project-details.blade.php
+++ resources/views/frontend/user/project-details.blade.php
@@ -18,6 +18,7 @@
                   </div>
                   <a href="{{route('testsuites.add',$project_details->id)}}" role="button" class="btn btn-primary">+ Test Suite</a>  
                   <a href="{{route('testcases.add',$project_details->id)}}" role="button" class="btn btn-primary">+ Test Case</a>
+                   
                 </div>
               </div>
 
@@ -75,9 +76,7 @@
                                       <path d="M5 12a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0 1A5 5 0 1 0 5 3a5 5 0 0 0 0 10z"/>
                                     </svg>
 
-                                      
                                       <div>{{strtoupper($suite->testsuite_name)}}</div>
-
 
                                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
