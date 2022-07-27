6709133c399d29d1b0475934abb4c151702fff4d
Shahin Islam Fakhrul <shahin2k5@gmail.com>
2022-07-27 13:00:17 +0600

    edit test case
    edit test case

--- app/Models/TestCase.php
+++ app/Models/TestCase.php
@@ -12,6 +12,6 @@ class TestCase extends Model
  
  
     protected $fillable = ['project_id','testsuite_id','project_code','testcase_name','testcase_precondition',
-                            'expected_result','testcase_steps','testcase_raw_details','testcase_steps_gherkins',
+                            'expected_result','testcase_steps','switch_steps_raw','testcase_raw_details','testcase_steps_gherkins',
                             'testcase_steps_classic','testcase_steps_and','project_admin','remarks'];
 }
--- database/migrations/2022_07_19_092509_edit_test_cases_table.php
+++ database/migrations/2022_07_19_092509_edit_test_cases_table.php
@@ -15,12 +15,49 @@ class EditTestCasesTable extends Migration
     {
         Schema::table('test_cases', function (Blueprint $table) {
 
-            $table->unsignedBigInteger('testsuite_id')->index()->nullable()->change();
-            $table->text('testcase_raw_details')->nullable();
-            $table->text('testcase_steps_gherkins')->nullable();
-            $table->text('testcase_steps_classic')->nullable();
+            if (Schema::hasColumn('test_cases', 'testsuite_id'))
+            {
+                 
+            }else{
+                $table->unsignedBigInteger('testsuite_id')->index()->nullable()->change();
+            }
+
+            if (Schema::hasColumn('test_cases', 'testcase_raw_details'))
+            {
+                 
+            }else{
+                $table->text('testcase_raw_details')->nullable();
+            }
+
+            if (Schema::hasColumn('test_cases', 'testcase_steps_gherkins')){
+                 
+            }else{
+                $table->text('testcase_steps_gherkins')->nullable();
+            }
+
+            if (Schema::hasColumn('test_cases', 'testcase_steps_classic')){
+                 
+            }else{
+                $table->text('testcase_steps_classic')->nullable();
+            }
+
+            if (Schema::hasColumn('test_cases', 'testcase_steps_classic')){
+                 
+            }else{
+                $table->text('testcase_steps_classic')->nullable();
+            }
+
+
+            if (Schema::hasColumn('test_cases', 'switch_steps_raw')){
+                 
+            }else{
+                $table->text('switch_steps_raw')->nullable()->after('testcase_steps');
+            }
             
+            $table->dropForeign('testsuite_id');
             $table->foreign('testsuite_id')->references('id')->on('test_suites')->onDelete('cascade');
+            
+            
             
         });
     }
